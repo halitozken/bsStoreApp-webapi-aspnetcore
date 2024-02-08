dc60fff1c2ba83c1fbe77f089070f39c95d89574
Halit Özken <halitozken@gmail.com>
2024-02-08 11:37:39 +0300

    global error management

--- Services/BookManager.cs
+++ Services/BookManager.cs
@@ -1,11 +1,8 @@
-﻿using Entities.Models;
+﻿using Entities.Exceptions;
+using Entities.Models;
 using Repositories.Contracts;
 using Services.Contracts;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Services
 {
@@ -13,6 +10,7 @@ namespace Services
     {
         private readonly IRepositoryManager _manager;
         private readonly ILoggerService _logger;
+        
 
         public BookManager(IRepositoryManager manager, ILoggerService logger)
         {
@@ -34,11 +32,7 @@ namespace Services
             var entity = _manager.Book.GetOneBookById(id, trackChanges);
 
             if (entity is null)
-            {
-                string message = $"The book with id:{id} could not found";
-                _logger.LogInfo(message);
-                throw new Exception(message);
-            }
+                throw new BookNotFoundException(id);
                 
 
             _manager.Book.DeleteOneBook(entity);
@@ -52,7 +46,12 @@ namespace Services
 
         public Book GetOneBookById(int id, bool trackChanges)
         {
-           return _manager.Book.GetOneBookById(id, trackChanges);
+           var book = _manager.Book.GetOneBookById(id, trackChanges);
+
+            if (book is null)
+                throw new BookNotFoundException(id);
+
+            return book;
         }
 
         public void UpdateOneBook(int id, Book book, bool trackChanges)
@@ -60,13 +59,7 @@ namespace Services
             // check entity
             var entity = _manager.Book.GetOneBookById(id, trackChanges);
             if (entity is null)
-            {
-                string message = $"Book with id:{id} could not found.";
-                _logger.LogInfo(message);
-                throw new Exception(message);
-            }
-                
-
+                throw new BookNotFoundException(id);
 
             entity.Title = book.Title;
             entity.Price = book.Price;
